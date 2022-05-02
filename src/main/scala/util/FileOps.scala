75d2ecba162fee833b781ee2bd6a7a9ccefbdb50
nghurtch <nghurtch@broadinstitute.com>
2022-05-02 23:02:42 +0400

    scalevolvable | make FileOps.saveFile IO composable

--- src/main/scala/util/FileOps.scala
+++ src/main/scala/util/FileOps.scala
@@ -4,13 +4,14 @@ import io.{IO, Writer}
 import parser.validator.ContainerValidatorSyntax.ArrayValidatorOps
 
 import java.io.{File, PrintWriter}
-import scala.util.{Using}
+import scala.util.Using
 
 
 object FileOps {
   def saveFile(args: Array[String], data: String)(implicit writer: Writer): IO[Unit] = {
-    val userHomeDir: String = System.getProperty("user.home")
-    val filePath: String = args.extractRequestParam("<o>").fold(s"$userHomeDir/data.txt")(_.value)
-    IO(Using(new PrintWriter(new File(filePath)))(_ write data))
+    for {
+      userHomeDir <- IO(System.getProperty("user.home"))
+      filePath <- IO(args.extractRequestParam("<o>").fold(s"$userHomeDir/data.txt")(_.value))
+    } yield Using(new PrintWriter(new File(filePath)))(_ write data)
   }
 }
